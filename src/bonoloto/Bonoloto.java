78beb0733fbbeadd16737e09bdc115aad0909377
BraisDiaz <braisdiaz@hotmail.com>
2017-05-16 09:41:34 +0200

    Boletin 26 en proceso

---
+++ src/bonoloto/Bonoloto.java
@@ -0,0 +1,21 @@
+
+package bonoloto;
+
+/**
+ *
+ * @author Brais Núñez
+ */
+public class Bonoloto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Ventana ven = new Ventana();
+        ven.setVisible(true);
+        ven.setLocationRelativeTo(null);
+                
+    }
+    
+}
